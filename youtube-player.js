0d22861541da1a69b9ff3e5877080a108e1468d6
GabrielTK <enricorocha1@hotmail.com>
2018-08-25 12:52:42 -0300

    Seems to fix some errors

--- youtube-player.js
+++ youtube-player.js
@@ -1,264 +1,132 @@
-const BasePlayer = require('./base-player');
-const Discord = require('discord.js');
-
-module.exports = class YoutubePlayer extends BasePlayer
-{
-    constructor(youtube)
-    {
-        super(youtube);
-        this.messages = new Map();
-        this._registerListener();
-    }
-
-    /**
-     *
-     * @param guild
-     * @param message
-     */
-    savePlayerMessage(guild, message)
-    {
-        this.messages.set(guild.id, message);
-    }
-
-    /**
-     * method used to play music queue tracks only
-     * @param guild
-     * @returns {*}
-     */
-    async play(guild)
-    {
-        let queue = this._queue.get(guild.id);
-        let connection = guild.voiceConnection;
-        let state = this._state.get(guild.id);
-        let timeout = this._timeouts.get(guild.id);
-
-        if (connection.channel.members.size === 1) {
-            connection.disconnect();
-            return this.emit('play', 'Music player stopped. No people in voice channel. Disconnecting...');
+const { Command } = require('discord.js-commando');
+//const {ExtendedCommand} = require('../../core/')
+const Youtube = require('discord-helpers/integrations/youtube');
+const Util = require('discord-helpers/util');
+/**
+* Command responsible for playing whatever is saved in memory for given guild
+* @type {module.PlayCommand}
+*/
+module.exports = class PlayCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'play',
+            aliases: ['listen', 'stream'],
+            group: 'music',
+            memberName: 'play',
+            description: 'Plays loaded queue',
+            examples: ['play'],
+            guildOnly: true,
+            clientPermissions: ['CONNECT', 'SPEAK'],
+            args: [
+                {
+                    key: 'query',
+                    prompt: 'Youtube URL or a song search text. E.g. `daft punk get lucky` or `https://www.youtube.com?v=qODMVRDehXE`',
+                    type: 'string',
+                    default: "abortthesearch"
+                }
+            ]
+        });
+        
+        try {
+            this._initListeners();
+        } catch (e) {
+            console.log('Failed to initialize PlayCommand listeners', e);
         }
-
-        if (timeout && timeout.count > 5) return this.emit('play', 'Music player has shut itself down due to failing to play track(s) for too long. Please make sure your music queue is not corrupted.', guild);
-        if (!state) state = this._initDefaultState(guild.id);
-
-        if (!queue || !queue.tracks || queue.tracks.length === 0) return this.emit('play', 'Queue for given guild is empty.', guild);
-        if (!connection) return this.emit('play', 'Not connected to voice channel for given guild.', guild);
-
-        if (connection.dispatcher && connection.dispatcher.paused) return this.emit('play', 'Music played is paused. Please resume playback or stop it before trying to play it.', guild);
-        else if (connection.dispatcher) connection.dispatcher.destroy('play', 'New dispatcher initialized');
-
-        if (queue.queue_end_reached === true && state.loop === true) {
-            if (state.shuffle === true) this.shuffle(guild);
-            this._resetQueuePosition(guild.id);
-            queue = this._queue.get(guild.id);
-        } else if (queue.queue_end_reached === true && state.loop === false) return this.emit('play', 'Music has finished playing for given guild. Looping is not enabled.', guild);
-
-        let track = this._getTrack(queue);
-        if (!state.seek) await this._youtube.download(track.url, `${YoutubePlayer.DOWNLOAD_DIR()}/${guild.id}`);
-        let dispatcher = connection.playFile(`${YoutubePlayer.DOWNLOAD_DIR()}/${guild.id}`, {seek: state.seek, volume: state.volume, passes: 2});
-
-        dispatcher.on('start', () => {
-            state.seek = 0;
-            this._state.set(guild.id, state);
-            this._timeouts.delete(guild.id);
-            this.emit('playing', track, guild);
-        });
-
-        dispatcher.on('end', (reason) => {
-            if (state.stop === false) {
-                this._TryToIncrementQueue(guild.id);
-                return this.play(guild);
-            } else {
-                state.stop = false;
-                this._state.set(guild.id, state);
+    }
+    
+    /**
+    *
+    * @param msg
+    * @returns {Promise.<Message|Message[]>}
+    */
+    async run(msg, {query}) {
+        this.rChannel = msg.channel;
+        if(query != 'abortthesearch'){
+            let loaderMsg;
+            try {
+                let indicatorMsg = (await msg.say('Searching for music. Please be patient.'));
+                loaderMsg = await Util.constructLoadingMessage(await msg.say(':hourglass_flowing_sand:'), ':hourglass_flowing_sand:');
+                
+                let results = await this.client.youtube.search(query);
+                indicatorMsg.delete();
+                loaderMsg.delete();
+                
+                if (results.length === 0) return (await msg.say(`Couldnt find any songs for query: \`${args.query}\`. Please make sure the link is correct and try again.`)).delete(5000);
+                
+                if (results.length > 50 || results.length === 1) {
+                    this.client.music.loadTracks(results, msg.guild, msg.author.id);
+                    (await msg.say(`${results.length} track(s) have been added to the music queue.`)).delete(12000)
+                } else {
+                    this.client.music.loadTracks([results[0]], msg.guild, msg.author.id);
+                    (await msg.say(`1 track(s) have been added to the music queue.`))
+                    await msg.say("We got more than one result. Please use `!get` for specific selection")
+                    
+                }
+            } catch (e) {
+                if (loaderMsg && typeof loaderMsg.delete === 'function') loaderMsg.delete();
+                console.log(e);
+                await msg.say('Something went horribly wrong! Please try again later.')
             }
-        });
-
-        dispatcher.on('error', e => {
-            console.log('error', e);
-            this._incrementTimeout(guild.id);
-            this._TryToIncrementQueue(guild.id);
-            return this.emit('play', `Music player encountered an error trying to play \`${track.title}\`.`, guild);
-        });
-    }
-
-    /**
-     * @param guild
-     */
-    shuffle(guild)
-    {
-        let queue = this._queue.get(guild.id);
-        if (queue) {
-            if (queue.tracks.length >= 2) {
-                queue.tracks = this._randomizeArray(queue.tracks);
-                this._queue.set(guild.id, queue);
-                this.emit('shuffle', `Music Player has shuffled _${queue.tracks.length}_ records`, guild);
-            } else this.emit('shuffle', 'Music Player could not shuffle tracks - not enough tracks present', guild)
-        } else this.emit('shuffle', 'Music Player could not shuffle track list at the moment.', guild)
-    }
-
-    /**
-     * @param guild
-     */
-    pause(guild)
-    {
-        let connection = guild.voiceConnection;
-        if (connection && connection.dispatcher && connection.dispatcher.paused === false) {
-            connection.dispatcher.pause();
-            this.emit('pause', 'Music Player has been paused', guild)
-        } else this.emit('pause', 'Music Player could not be paused. Player is not connected or is already paused at the moment.', guild)
-    }
-
-    /**
-     * @param guild
-     */
-    resume(guild)
-    {
-        let connection = guild.voiceConnection;
-        if (connection && connection.dispatcher && connection.dispatcher.paused === true) {
-            connection.dispatcher.resume();
-            this.emit('resume', 'Music Player has been resumed', guild)
-        } else  this.emit('resume', 'Music Player could not be resumed. Player is not connected or is not paused at the moment.', guild)
-    }
-
-    /**
-     * @param guild
-     */
-    skip(guild)
-    {
-        let state = this._state.get(guild.id);
-        let connection = guild.voiceConnection;
-        if (state && connection && (connection.dispatcher || connection.speaking === true)) {
-            connection.dispatcher.end('skip() method initiated');
-            return this.emit('skip', 'Music player is skipping.', guild)
-        } else this.emit('skip', 'Music Player could not skip track at the moment. Player not connected or is not playing anything yet.', guild)
-    }
-
-    /**
-     * @param guild
-     * @param timeInSeconds
-     * @param timeString
-     */
-    seek(guild, timeInSeconds, timeString)
-    {
-        let connection = guild.voiceConnection;
-        let state = this._state.get(guild.id);
-        if (state && connection && connection.dispatcher) {
-            state.increment_queue = false;
-            state.seek = timeInSeconds;
-            this._state.set(guild.id, state);
-
-            connection.dispatcher.end('seek() method initiated');
-            this.emit('seek', `Seeking player to \`${timeString}\``, guild);
-        } else this.emit('seek', 'Player could not seek. Player not connected or is not playing music at the moment.', guild);
-    }
-
-    /**
-     * @param guild
-     * @param position
-     * @returns {Emitter|*}
-     */
-    jump(guild, position)
-    {
-        let connection = guild.voiceConnection;
-        let queue = this._queue.get(guild.id);
-        let state = this._state.get(guild.id);
-
-        if (connection && connection.dispatcher) {
-            if (queue.tracks.length === 0 || queue.tracks.length < position-1)
-                return this.emit('info', `Incorrect song number provided. Allowed 1-${queue.tracks.length}]`);
-
-            state.increment_queue = false;
-            this._state.set(guild.id, state);
-
-            queue.position = position - 1;
-            this._queue.set(guild.id, queue);
-
-            connection.dispatcher.end('jump() method initiated');
-
-            this.emit('jump', `Player jumping to play track \`#${position} [${queue.tracks[position-1].title}]\``, guild);
-        } else this.emit('jump', 'Player could jump to specific song. Player not connected or is not playing music at the moment.', guild);
-    }
-
-    /**
-     * @param guild
-     */
-    stop(guild)
-    {
-        let state = this._state.get(guild.id);
-        let connection = guild.voiceConnection;
-        if (connection && connection.dispatcher) {
-            if (state) {
-                state.stop = true;
-                this._state.set(guild.id, state);
-                this.emit('stop', 'Music player has been stopped.', guild);
-                this.emit('update', guild, true);
-            }
-            connection.dispatcher.destroy('stop', 'manual dispatcher destroy init');
-        } else this.emit('stop', 'Music Player could not be stopped. Player not connected.', guild)
-    }
-
-    /**
-     *
-     * @param guild
-     * @param volume
-     */
-    setVolume(guild, volume)
-    {
-        let connection = guild.voiceConnection;
-        let state = this._state.get(guild.id);
-        if (connection && connection.dispatcher) {
-            connection.dispatcher.setVolume(volume / 100.0);
-            state.volume = volume / 100.0;
-            this._state.set(guild.id, state);
-            this.emit('volume', `Music player volume has been set to \`${volume}\``, guild);
-            this.emit('update', guild);
-        } else this.emit('volume', 'Music Player is not active at the moment. Can not set volume.', guild)
-    }
-
-    /**
-     * Listener tracks important music player changes and updates player message accordingly
-     * @private
-     */
-    _registerListener()
-    {
-        this.on('update', async (guild, stopped = false) => {
-            let channel;
-            let message = this.messages.get(guild.id);
-            if (message && message.deletable) {
-                channel = message.channel;
-                message.delete();
-            } if (stopped === false && message && channel) {
-                this.messages.set(guild.id, await channel.send('', {embed: this.getInfo(guild)}));
-            } else if (stopped === true) {
-                this.messages.delete(guild.id);
-            }
-        })
-    }
-
-    /**
-     * Gets music player info
-     * @param guild
-     * @returns {*}
-     */
-    getInfo(guild)
-    {
-        let connection = guild.voiceConnection;
-        if (connection && connection.dispatcher) {
-            let queue = this._queue.get(guild.id);
-            let track = queue.tracks[queue.position];
-            let embed = new Discord.RichEmbed();
-            embed
-                .setAuthor(`Playing - 🎵 ${track.title} | ${track.source} 🎵`, track.image, track.url)
-                .setColor('RANDOM')
-                .addField('Song Number', `${track.position+1} / ${track.total}`, true)
-                .addField('Duration', `${track.duration}`, true)
-                .addField('Volume', `${connection.dispatcher.volume * 100} %`, true)
-                .addField('Requested By', guild.members.get(track.added_by) || 'Unknown', true)
-                .setImage(track.image)
-                .setTimestamp();
-            return embed;
         }
-        return null;
+        
+        
+        
+        
+        
+        if(msg.guild.voiceConnection == null){
+            try {
+                let user = msg.member;
+                if (!user.voiceChannel) return (await msg.say('You must join voice channel first before using this command')).delete(12000);
+                else {
+                    if (user.voiceChannel.joinable){
+                        var connection = await user.voiceChannel.join();
+                        (await msg.say(`Joined Voice Channel - \`${connection.channel.name}\``)).delete(12000);
+                    }
+                    else return msg.say(`I can't join channel ${user.voiceChannel.name}. Missing permissions.`)
+                }
+            } catch (e) {
+                console.log(e);
+                return msg.say('Something went horribly wrong! Please try again later.')
+            }
+        }
+        try {
+            await this.client.music.play(msg.guild);
+            debugger;
+        } catch (e) {
+            console.log(e);
+            return msg.say('Something went horribly wrong! Please try again later.')
+        }
     }
-
+    
+    _initListeners()
+    {
+        
+        this.client.music.on('playing', async (track, guild) => {
+            var channel = this.rChannel;
+            if(typeof channel == 'undefined'){
+                target = guild.channels.find("name", "musica")
+            }
+            if (guild.voiceConnection) {
+                let playingMessage = this.client.music.messages.get(guild.id);
+                if (playingMessage && playingMessage.deletable) playingMessage.delete();
+                
+                if (channel) this.client.music.savePlayerMessage(guild, (await channel.send({embed: this.client.music.getInfo(guild)})));
+                else console.log(`No text channel found for guild ${guild.id}/${guild.name} to display music playing embed.`)
+            }
+        });
+        
+        this.client.music.on('play', (text, guild) => {
+            var channel = this.rChannel;
+            if(typeof channel == 'undefined'){
+                target = guild.channels.find("name", "musica")
+            }
+            if (guild.voiceConnection) {
+                
+                if (channel) channel.send(text);
+                else console.log(`No text channel found for guild ${guild.id}/${guild.name} to display music playing embed.`)
+            }
+        });
+        
+        this.client.music.on('error', text => { throw text; });
+    }
 };
